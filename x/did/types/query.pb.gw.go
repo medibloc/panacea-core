e11a75d4da0e70b79f89d1196c3f4461cb791186
gyuguen <gyuguen.jang@medibloc.org>
2021-06-03 17:46:49 +0900

    Modify proto files and build *.pb.go

--- x/did/types/query.pb.gw.go
+++ x/did/types/query.pb.gw.go
@@ -181,7 +181,7 @@ func RegisterQueryHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 }
 
 var (
-	pattern_Query_DIDDocumentWithSeq_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"medibloc", "panaceacore", "did", "DID"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Query_DIDDocumentWithSeq_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "v1", "did", "DID"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
