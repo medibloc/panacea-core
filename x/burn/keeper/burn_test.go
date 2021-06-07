e3057f0dfe7cc652090fcff90530c29938ab48f9
gyuguen <gyuguen.jang@medibloc.org>
2021-06-07 19:01:29 +0900

    Change burn module's test to common test module

--- x/burn/keeper/burn_test.go
+++ x/burn/keeper/burn_test.go
@@ -1,28 +1,13 @@
 package keeper_test
 
 import (
-	"github.com/cosmos/cosmos-sdk/codec"
-	"github.com/cosmos/cosmos-sdk/codec/types"
-	"github.com/cosmos/cosmos-sdk/store"
-	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
-	"github.com/cosmos/cosmos-sdk/x/auth/tx"
-	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
-	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
-	paramskeeper "github.com/cosmos/cosmos-sdk/x/params/keeper"
-	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
-	"github.com/medibloc/panacea-core/x/burn/keeper"
-	"github.com/tendermint/tendermint/libs/log"
-	"github.com/tendermint/tendermint/types/time"
-	dbm "github.com/tendermint/tm-db"
+	"github.com/medibloc/panacea-core/types/testsuite"
 	"testing"
 
-	"github.com/stretchr/testify/suite"
-	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/suite"
 
-	"github.com/medibloc/panacea-core/app/params"
 	"github.com/medibloc/panacea-core/types/assets"
 	"github.com/tendermint/tendermint/crypto"
 	"github.com/tendermint/tendermint/crypto/secp256k1"
@@ -42,82 +27,21 @@ var (
 	}
 
 	initTokens = sdk.TokensFromConsensusPower(200)
-	initCoin = sdk.NewCoin(assets.MicroMedDenom, initTokens)
+	initCoin   = sdk.NewCoin(assets.MicroMedDenom, initTokens)
 	initCoins  = sdk.NewCoins(initCoin)
 )
 
-func newTestCodec() params.EncodingConfig {
-	cdc := codec.NewLegacyAmino()
-	interfaceRegistry := types.NewInterfaceRegistry()
-	banktypes.RegisterInterfaces(interfaceRegistry)
-	marshaler := codec.NewProtoCodec(interfaceRegistry)
-
-	return params.EncodingConfig{
-		InterfaceRegistry: interfaceRegistry,
-		Marshaler:         marshaler,
-		TxConfig:          tx.NewTxConfig(marshaler, tx.DefaultSignModes),
-		Amino:             cdc,
-	}
-
+type BurnTestSuite struct {
+	testsuite.TestSuite
 }
 
-type TestInput struct {
-	suite.Suite
-
-	Ctx           sdk.Context
-
-	AccountKeeper authkeeper.AccountKeeper
-	BankKeeper    bankkeeper.Keeper
-	Keeper        keeper.Keeper
+func TestKeeperTestSuite(t *testing.T) {
+	suite.Run(t, new(BurnTestSuite))
 }
 
-func (suite *TestInput) SetupTest() {
-	keyBank := sdk.NewKVStoreKey(banktypes.StoreKey)
-	keyParams := sdk.NewKVStoreKey(paramstypes.StoreKey)
-	tKeyParams := sdk.NewTransientStoreKey(paramstypes.TStoreKey)
-
-	cdc := newTestCodec()
-	db := dbm.NewMemDB()
-	ms := store.NewCommitMultiStore(db)
-	ctx := sdk.NewContext(ms, tmproto.Header{Time: time.Now()}, false, log.NewNopLogger())
-
-	ms.MountStoreWithDB(keyBank, sdk.StoreTypeIAVL, db)
-	ms.MountStoreWithDB(tKeyParams, sdk.StoreTypeTransient, db)
-	ms.MountStoreWithDB(keyParams, sdk.StoreTypeIAVL, db)
-
-	suite.Require().NoError(ms.LoadLatestVersion())
-
-	maccPerms := map[string][]string{
-		authtypes.FeeCollectorName: nil,
-	}
-
-	modAccAddrs := make(map[string]bool)
-	for acc := range maccPerms {
-		modAccAddrs[authtypes.NewModuleAddress(acc).String()] = true
-	}
-
-	paramsKeeper := paramskeeper.NewKeeper(
-		cdc.Marshaler,
-		cdc.Amino,
-		keyParams,
-		tKeyParams)
-	accountKeeper := authkeeper.NewAccountKeeper(
-		cdc.Marshaler,
-		keyBank,
-		paramsKeeper.Subspace(authtypes.ModuleName),
-		authtypes.ProtoBaseAccount,
-		maccPerms,
-	)
-	accountKeeper.SetParams(ctx, authtypes.DefaultParams())
-	bankKeeper := bankkeeper.NewBaseKeeper(
-		cdc.Marshaler,
-		keyBank,
-		accountKeeper,
-		paramsKeeper.Subspace(banktypes.ModuleName),
-		modAccAddrs,
-	)
-	bankKeeper.SetParams(ctx, banktypes.DefaultParams())
-	burnKeeper := keeper.NewKeeper(bankKeeper)
+func (suite *BurnTestSuite) BeforeTest(_, _ string) {
+	ctx := suite.Ctx
+	bankKeeper := suite.BankKeeper
 
 	for _, addr := range address {
 		err := bankKeeper.AddCoins(ctx, addr, initCoins)
@@ -134,19 +58,12 @@ func (suite *TestInput) SetupTest() {
 	suite.Require().Equal(1, len(totalCoins))
 	suite.Require().Equal(assets.MicroMedDenom, totalCoins[0].Denom)
 	suite.Require().Equal(sdk.TokensFromConsensusPower(600), totalCoins[0].Amount)
-
-	suite.Ctx = ctx
-	suite.AccountKeeper = accountKeeper
-	suite.BankKeeper = bankKeeper
-	suite.Keeper = *burnKeeper
-
-
 }
 
-func (suite *TestInput) TestBurnCoins() {
+func (suite *BurnTestSuite) TestBurnCoins() {
 	ctx := suite.Ctx
 	bankKeeper := suite.BankKeeper
-	burnKeeper := suite.Keeper
+	burnKeeper := suite.BurnKeeper
 
 	beforeBurnCoin1 := bankKeeper.GetBalance(ctx, address[0], assets.MicroMedDenom)
 	beforeBurnCoin2 := bankKeeper.GetBalance(ctx, address[1], assets.MicroMedDenom)
@@ -175,10 +92,10 @@ func (suite *TestInput) TestBurnCoins() {
 	suite.Require().Equal(sdk.TokensFromConsensusPower(400), afterBurnTotal[0].Amount)
 }
 
-func (suite *TestInput)TestGetAccount_NotExistAddress() {
+func (suite *BurnTestSuite) TestGetAccount_NotExistAddress() {
 	ctx := suite.Ctx
 	bankKeeper := suite.BankKeeper
-	burnKeeper := suite.Keeper
+	burnKeeper := suite.BurnKeeper
 
 	beforeBurnCoin1 := bankKeeper.GetBalance(ctx, address[0], assets.MicroMedDenom)
 	beforeBurnCoin2 := bankKeeper.GetBalance(ctx, address[1], assets.MicroMedDenom)
@@ -206,8 +123,4 @@ func (suite *TestInput)TestGetAccount_NotExistAddress() {
 	suite.Require().Equal(assets.MicroMedDenom, afterBurnTotal[0].GetDenom())
 	suite.Require().Equal(sdk.TokensFromConsensusPower(600), afterBurnTotal[0].Amount)
 
-}
-
-func TestKeeperTestSuite(t *testing.T) {
-	suite.Run(t, new(TestInput))
 }
