2555b3e461ac6791d0effae82511c028c399eb60
gyuguen <gyuguen.jang@medibloc.org>
2021-06-14 14:34:48 +0900

    Change did rest api url (#138)
    Change did error code start to 2

--- x/did/types/query.pb.gw.go
+++ x/did/types/query.pb.gw.go
@@ -181,7 +181,7 @@ func RegisterQueryHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 }
 
 var (
-	pattern_Query_DIDDocumentWithSeq_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "v1", "did", "DID"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Query_DIDDocumentWithSeq_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"panacea", "did", "v2", "dids", "DID"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
