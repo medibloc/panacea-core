6fbd3bdc55399f4999d3acb290f86ecf3faec442
InChul <49394875+inchori@users.noreply.github.com>
2022-01-17 10:37:40 +0900

    feat: Add Query Single Deal for Data Market Module (#253)

--- x/market/keeper/grpc_query_deal.go
+++ x/market/keeper/grpc_query_deal.go
@@ -13,10 +13,13 @@ func (k Keeper) Deal(goCtx context.Context, req *types.QueryDealRequest) (*types
 	if req == nil {
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
+
 	ctx := sdk.UnwrapSDKContext(goCtx)
+
 	deal, err := k.GetDeal(ctx, req.DealId)
 	if err != nil {
 		return nil, sdkerrors.ErrKeyNotFound
 	}
+
 	return &types.QueryDealResponse{Deal: &deal}, nil
 }
