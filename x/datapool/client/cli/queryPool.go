2c277b44384351d97f0fd415bc424fc4d8e5c417
Song Inchul <sic61695@gmail.com>
2022-05-25 18:24:29 +0900

    fix typo

--- x/datapool/client/cli/queryPool.go
+++ x/datapool/client/cli/queryPool.go
@@ -115,7 +115,7 @@ func CmdGetDataValidationCertificates() *cobra.Command {
 		},
 	}
 
-	flags.AddPaginationFlagsToCmd(cmd, "all data certificate by pool ID and round")
+	flags.AddPaginationFlagsToCmd(cmd, "all data certificates by pool ID and round")
 	flags.AddQueryFlagsToCmd(cmd)
 
 	return cmd
