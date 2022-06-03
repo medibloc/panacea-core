f7a8f963e5e5bbc94ff82bdae67db65b3fd3a2c6
InChul <49394875+inchori@users.noreply.github.com>
2022-06-03 14:53:16 +0900

    feat: Validate Update Oracle Requester

--- x/oracle/types/errors.go
+++ x/oracle/types/errors.go
@@ -5,6 +5,7 @@ import sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 // DONTCOVER
 
 var (
-	ErrOracleNotFound     = sdkerrors.Register(ModuleName, 1, "oracle not found")
-	ErrOracleAlreadyExist = sdkerrors.Register(ModuleName, 2, "oracle already exists")
+	ErrOracleNotFound         = sdkerrors.Register(ModuleName, 1, "oracle not found")
+	ErrOracleAlreadyExist     = sdkerrors.Register(ModuleName, 2, "oracle already exists")
+	ErrInvalidUpdateRequester = sdkerrors.Register(ModuleName, 3, "invalid update requester")
 )
