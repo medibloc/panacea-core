e7d0632ec5cede424cb1dfce5371424f4780f438
InChul <49394875+inchori@users.noreply.github.com>
2022-06-10 11:20:20 +0900

    fix: Fix Oracle Genesis State (#351)

--- x/oracle/types/genesis.go
+++ x/oracle/types/genesis.go
@@ -1,5 +1,10 @@
 package types
 
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
 // DefaultGenesis returns the default Capability genesis state
 func DefaultGenesis() *GenesisState {
 	return &GenesisState{
@@ -10,6 +15,11 @@ func DefaultGenesis() *GenesisState {
 // Validate performs basic genesis state validation returning an error upon any
 // failure.
 func (gs GenesisState) Validate() error {
-
+	for _, oracle := range gs.Oracles {
+		oracleAddr, err := sdk.AccAddressFromBech32(oracle.Address)
+		if err != nil {
+			return sdkerrors.Wrapf(ErrInvalidOracleAccAddress, "oracle address: %s", oracleAddr.String())
+		}
+	}
 	return nil
 }
