69dc9887f6c40d0f0ac8b704ee4f7409957e859e
hansol-medi <hansol@medibloc.org>
2022-07-19 10:55:29 +0900

    fix oracle proto

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -17,7 +17,7 @@ protoc_gen_gocosmos() {
 
 protoc_gen_gocosmos
 
-proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -not -path '*/v2alpha2/*' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   buf protoc \
   -I "proto" \
