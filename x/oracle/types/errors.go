aff37400b457bf65cf5bed3081a14e811882dcd3
gyuguen <gyuguen.jang@medibloc.org>
2022-07-28 15:44:39 +0900

    fix

--- x/oracle/types/errors.go
+++ x/oracle/types/errors.go
@@ -3,7 +3,6 @@ package types
 import sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 // DONTCOVER
-
 var (
 	ErrOracleRegistrationVote      = sdkerrors.Register(ModuleName, 1, "error while voting for OracleRegistration")
 	ErrDetectionMaliciousBehavior  = sdkerrors.Register(ModuleName, 2, "errors caused by malicious actions")
