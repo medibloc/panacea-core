21ebba4378e5a2192cf39c7d3b4c0177cdd4530e
InChul <49394875+inchori@users.noreply.github.com>
2022-08-16 16:06:04 +0900

    fix

--- x/oracle/types/event.go
+++ x/oracle/types/event.go
@@ -1,17 +1,8 @@
 package types
 
 const (
-<<<<<<< HEAD
-	AttributeKeyRegisterOracle   = "oracle"
-	AttributeValueRegisterOracle = "RegisterOracleEvent"
-)
-
-var (
-	EventTypeRegisterOracle = "register"
-=======
 	EventTypeRegisterOracle = "register"
 
 	AttributeKeyOracle   = "oracle"
 	AttributeValueOracle = "RegisterOracleEvent"
->>>>>>> origin/ft/na/emit_event_register_oracle
 )
