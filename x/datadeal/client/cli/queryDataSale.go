510ea558629a03a1ed5b0a6c45b0c497d2993b6b
InChul <49394875+inchori@users.noreply.github.com>
2022-09-28 18:21:21 +0900

    feat: add sell data Tx and query a datasale CLI (#422)

---
+++ x/datadeal/client/cli/queryDataSale.go
@@ -0,0 +1,46 @@
+package cli
+
+import (
+	"strconv"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/medibloc/panacea-core/v2/x/datadeal/types"
+	"github.com/spf13/cobra"
+)
+
+func GetCmdDataSale() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "datasale [dealID] [verifiableCID]",
+		Short: "Query a datasale",
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			dealID, err := strconv.ParseUint(args[0], 10, 64)
+			if err != nil {
+				return err
+			}
+
+			req := &types.QueryDataSaleRequest{
+				DealId:        dealID,
+				VerifiableCid: args[1],
+			}
+			res, err := queryClient.DataSale(cmd.Context(), req)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
