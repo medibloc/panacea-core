8dc2c71d5e7b510096b2613a950d690d87d0f43a
Hansol Lee <38912532+H4NLee@users.noreply.github.com>
2022-10-14 15:08:10 +0900

    feat: add oracle reward event (#455)

--- x/oracle/types/events.go
+++ x/oracle/types/events.go
@@ -2,6 +2,7 @@ package types
 
 const (
 	EventTypeRegistrationVote = "oracle_registration"
+	EventTypeOracleReward     = "oracle_reward"
 
 	AttributeKeyVoteStatus          = "vote_status"
 	AttributeKeyOracleAddress       = "oracle_address"
