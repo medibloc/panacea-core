1c32ec0a7bab09b9a250e2e54d2727c35f40cf05
audtlr24 <audtlr24@gmail.com>
2022-12-05 15:24:54 +0900

    fix

--- x/oracle/handler.go
+++ x/oracle/handler.go
@@ -11,12 +11,21 @@ import (
 
 // NewHandler ...
 func NewHandler(k keeper.Keeper) sdk.Handler {
-	//msgServer := keeper.NewMsgServerImpl(k)
+	msgServer := keeper.NewMsgServerImpl(k)
 
 	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
 		ctx = ctx.WithEventManager(sdk.NewEventManager())
 
 		switch msg := msg.(type) {
+		case *types.MsgRegisterOracle:
+			res, err := msgServer.RegisterOracle(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
+		//case *types.MsgApproveOracleRegistration:
+		//	res, err := msgServer.ApproveOracleRegistration(sdk.WrapSDKContext(ctx), msg)
+		//	return sdk.WrapServiceResult(ctx, res, err)
+		//case *types.MsgUpdateOracleInfo:
+		//	res, err := msgServer.UpdateOracleInfo(sdk.WrapSDKContext(ctx), msg)
+		//	return sdk.WrapServiceResult(ctx, res, err)
 		default:
 			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
 			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, errMsg)
