34d058ba6643351d7374aeccea205594c48dc375
InChul <49394875+inchori@users.noreply.github.com>
2022-12-06 15:15:08 +0900

    feat: add skeleton code for datadeal module

---
+++ x/datadeal/types/message_deal.go
@@ -0,0 +1,31 @@
+package types
+
+import sdk "github.com/cosmos/cosmos-sdk/types"
+
+var _ sdk.Msg = &MsgCreateDeal{}
+
+func (m *MsgCreateDeal) Route() string {
+	return RouterKey
+}
+
+func (m *MsgCreateDeal) Type() string {
+	return "RegisterOracle"
+}
+
+func (m *MsgCreateDeal) ValidateBasic() error {
+
+	return nil
+}
+
+func (m *MsgCreateDeal) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(m)
+	return sdk.MustSortJSON(bz)
+}
+
+func (m *MsgCreateDeal) GetSigners() []sdk.AccAddress {
+	buyerAddress, err := sdk.AccAddressFromBech32(m.BuyerAddress)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{buyerAddress}
+}
