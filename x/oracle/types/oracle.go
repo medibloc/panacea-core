08e5139251b01ec846a69852fe517f965e4bc269
H4NLee <hansol@medibloc.org>
2022-12-15 09:27:03 +0900

    review applied

--- x/oracle/types/oracle.go
+++ x/oracle/types/oracle.go
@@ -86,16 +86,12 @@ func (m *OracleRegistration) ValidateBasic() error {
 		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "trusted block hash should not be nil")
 	}
 
-	if m.OracleCommissionRate.LT(sdk.ZeroDec()) || m.OracleCommissionRate.GT(sdk.OneDec()) {
-		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "oracleCommissionRate must be between 0 and 1")
-	}
-
 	if m.OracleCommissionMaxRate.LT(sdk.ZeroDec()) || m.OracleCommissionMaxRate.GT(sdk.OneDec()) {
 		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "OracleCommissionMaxRate must be between 0 and 1")
 	}
 
-	if m.OracleCommissionRate.GT(m.OracleCommissionMaxRate) {
-		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "OracleCommissionRate cannot be greater than OracleCommissionMaxRate")
+	if m.OracleCommissionRate.LT(sdk.ZeroDec()) || m.OracleCommissionRate.GT(m.OracleCommissionMaxRate) {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "oracleCommissionRate must be between 0 and OracleCommissionMaxRate")
 	}
 
 	if m.OracleCommissionMaxChangeRate.LT(sdk.ZeroDec()) || m.OracleCommissionMaxChangeRate.GT(sdk.OneDec()) {
