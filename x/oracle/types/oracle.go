d38bf7b5ed34a3c7882729d3fc35cff2c516ee04
audtlr24 <audtlr24@gmail.com>
2022-12-19 16:36:35 +0900

    feat: add extra commission rate info to genoracle cmd (#559)

--- x/oracle/types/oracle.go
+++ x/oracle/types/oracle.go
@@ -28,15 +28,13 @@ func (m *Oracle) ValidateBasic() error {
 	if len(m.UniqueId) == 0 {
 		return fmt.Errorf("uniqueID is empty")
 	}
-	if len(m.Endpoint) == 0 {
-		return fmt.Errorf("endpoint is empty")
-	}
-	if m.OracleCommissionRate.LT(sdk.ZeroDec()) || m.OracleCommissionRate.GT(sdk.OneDec()) {
-		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "oracleCommissionRate must be between 0 and 1")
-	}
 
 	if m.OracleCommissionMaxRate.LT(sdk.ZeroDec()) || m.OracleCommissionMaxRate.GT(sdk.OneDec()) {
 		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "OracleCommissionMaxRate must be between 0 and 1")
+	}
+
+	if m.OracleCommissionRate.LT(sdk.ZeroDec()) || m.OracleCommissionRate.GT(m.OracleCommissionMaxRate) {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "oracleCommissionRate must be between 0 and OracleCommissionMaxRate")
 	}
 
 	if m.OracleCommissionMaxChangeRate.LT(sdk.ZeroDec()) || m.OracleCommissionMaxChangeRate.GT(sdk.OneDec()) {
