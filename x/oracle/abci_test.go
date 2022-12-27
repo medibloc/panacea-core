34fee1848278adedd41e254b732388156edd90ed
audtlr24 <audtlr24@gmail.com>
2022-12-27 14:58:39 +0900

    fix

--- x/oracle/abci_test.go
+++ x/oracle/abci_test.go
@@ -45,12 +45,14 @@ func (suite *abciTestSuite) TestOracleUpgradeSuccess() {
 
 	upgradeInfo := &types.OracleUpgradeInfo{
 		UniqueId: upgradeUniqueID,
-		Height:   ctx.BlockHeight(),
+		Height:   10,
 	}
 
 	suite.Require().NoError(suite.OracleKeeper.SetOracleUpgradeInfo(ctx, upgradeInfo))
 
-	oracle.EndBlocker(suite.Ctx, suite.OracleKeeper)
+	ctx = ctx.WithBlockHeight(10)
+
+	oracle.EndBlocker(ctx, suite.OracleKeeper)
 
 	suite.Require().Equal(upgradeUniqueID, suite.OracleKeeper.GetParams(ctx).UniqueId)
 
@@ -58,12 +60,24 @@ func (suite *abciTestSuite) TestOracleUpgradeSuccess() {
 	suite.Require().Error(err, types.ErrOracleUpgradeInfoNotFound)
 }
 
-func (suite *abciTestSuite) TestOracleUpgradeEmptyUpgradeData() {
+func (suite *abciTestSuite) TestOracleUpgradeFailedBeforeReachUpgradeHeight() {
 	ctx := suite.Ctx
+	ctx = ctx.WithBlockHeight(1)
 
 	suite.Require().Equal(suite.uniqueID, suite.OracleKeeper.GetParams(ctx).UniqueId)
 
-	oracle.EndBlocker(suite.Ctx, suite.OracleKeeper)
+	upgradeUniqueID := "upgradeUniqueID"
+
+	upgradeInfo := &types.OracleUpgradeInfo{
+		UniqueId: upgradeUniqueID,
+		Height:   10,
+	}
+
+	suite.Require().NoError(suite.OracleKeeper.SetOracleUpgradeInfo(ctx, upgradeInfo))
+
+	ctx = ctx.WithBlockHeight(9)
+
+	oracle.EndBlocker(ctx, suite.OracleKeeper)
 
 	suite.Require().Equal(suite.uniqueID, suite.OracleKeeper.GetParams(ctx).UniqueId)
 }
