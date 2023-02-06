e95d85075e8e60214180df439d191b5c5891d8eb
audtlr24 <audtlr24@gmail.com>
2023-02-06 18:06:18 +0900

    feat: poc

--- go.mod
+++ go.mod
@@ -14,8 +14,10 @@ require (
 	github.com/gorilla/mux v1.8.0
 	github.com/grpc-ecosystem/grpc-gateway v1.16.0
 	github.com/hyperledger/aries-framework-go v0.1.8
+	github.com/hyperledger/aries-framework-go/component/storageutil v0.0.0-20220322085443-50e8f9bd208b
 	github.com/lestrrat-go/jwx/v2 v2.0.8
 	github.com/pborman/uuid v1.2.0
+	github.com/piprate/json-gold v0.4.1-0.20210813112359-33b90c4ca86c
 	github.com/prometheus/client_golang v1.12.2
 	github.com/rakyll/statik v0.1.7
 	github.com/sirupsen/logrus v1.9.0
@@ -38,10 +40,12 @@ require (
 	github.com/ChainSafe/go-schnorrkel v0.0.0-20200405005733-88cbf1b4c40d // indirect
 	github.com/CosmWasm/wasmvm v1.0.0-beta7 // indirect
 	github.com/DataDog/zstd v1.4.5 // indirect
+	github.com/VictoriaMetrics/fastcache v1.5.7 // indirect
 	github.com/Workiva/go-datastructures v1.0.53 // indirect
 	github.com/armon/go-metrics v0.3.10 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/bgentry/speakeasy v0.1.0 // indirect
+	github.com/cenkalti/backoff/v4 v4.1.3 // indirect
 	github.com/cespare/xxhash v1.1.0 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/coinbase/rosetta-sdk-go v0.7.0 // indirect
@@ -73,6 +77,7 @@ require (
 	github.com/google/btree v1.0.1 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/orderedcode v0.0.1 // indirect
+	github.com/google/tink/go v1.6.1-0.20210519071714-58be99b3c4d0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/handlers v1.5.1 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
@@ -87,6 +92,7 @@ require (
 	github.com/hyperledger/aries-framework-go/spi v0.0.0-20220322085443-50e8f9bd208b // indirect
 	github.com/improbable-eng/grpc-web v0.14.1 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/jinzhu/copier v0.0.0-20190924061706-b57f9002281a // indirect
 	github.com/jmhodges/levigo v1.0.0 // indirect
 	github.com/keybase/go-keychain v0.0.0-20190712205309-48d3d31d256d // indirect
 	github.com/kilic/bls12-381 v0.1.1-0.20210503002446-7b7597926c69 // indirect
@@ -103,17 +109,20 @@ require (
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
 	github.com/mimoo/StrobeGo v0.0.0-20181016162300-f8f6d4d2b643 // indirect
+	github.com/minio/blake2b-simd v0.0.0-20160723061019-3f5f724cb5b1 // indirect
 	github.com/minio/highwayhash v1.0.2 // indirect
+	github.com/minio/sha256-simd v0.1.1 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/mr-tron/base58 v1.1.3 // indirect
 	github.com/mtibben/percent v0.2.1 // indirect
 	github.com/multiformats/go-base32 v0.0.3 // indirect
 	github.com/multiformats/go-multibase v0.0.1 // indirect
+	github.com/multiformats/go-multihash v0.0.13 // indirect
+	github.com/multiformats/go-varint v0.0.5 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.2 // indirect
 	github.com/petermattis/goid v0.0.0-20180202154549-b0b1615b78e5 // indirect
-	github.com/piprate/json-gold v0.4.1-0.20210813112359-33b90c4ca86c // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/pquerna/cachecontrol v0.0.0-20180517163645-1555304b9b35 // indirect
@@ -125,6 +134,7 @@ require (
 	github.com/rs/cors v1.8.2 // indirect
 	github.com/rs/zerolog v1.27.0 // indirect
 	github.com/sasha-s/go-deadlock v0.2.1-0.20190427202633-1595213edefa // indirect
+	github.com/spaolacci/murmur3 v1.1.0 // indirect
 	github.com/spf13/afero v1.8.2 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/square/go-jose/v3 v3.0.0-20200630053402-0a67ce9b0693 // indirect
---
+++ x/did/client/poc_test.go
@@ -0,0 +1,249 @@
+package aries_test
+
+import (
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/btcsuite/btcutil/base58"
+	"github.com/hyperledger/aries-framework-go/component/storageutil/mem"
+	"github.com/hyperledger/aries-framework-go/pkg/doc/did"
+	"github.com/hyperledger/aries-framework-go/pkg/doc/ld"
+	"github.com/hyperledger/aries-framework-go/pkg/doc/signature/jsonld"
+	"github.com/hyperledger/aries-framework-go/pkg/doc/signature/signer"
+	"github.com/hyperledger/aries-framework-go/pkg/doc/signature/suite"
+	"github.com/hyperledger/aries-framework-go/pkg/doc/signature/suite/ecdsasecp256k1signature2019"
+	"github.com/hyperledger/aries-framework-go/pkg/doc/signature/verifier"
+	"github.com/hyperledger/aries-framework-go/pkg/framework/context"
+	ldstore "github.com/hyperledger/aries-framework-go/pkg/store/ld"
+	jsld "github.com/piprate/json-gold/ld"
+	"github.com/stretchr/testify/require"
+)
+
+func TestJsonDoc(t *testing.T) {
+
+	docJson := validDoc
+
+	doc, err := did.ParseDocument([]byte(docJson))
+	require.NoError(t, err)
+	require.NotNil(t, doc)
+
+	require.Equal(t, doc.ID, "did:example:21tDAKCERh95uGgKbJNHYp")
+
+	hexDecodeValue, err := hex.DecodeString("02b97c30de767f084ce3080168ee293053ba33b235d7116a3263d29f1450936b71")
+	eAuthentication := []did.Verification{
+		{VerificationMethod: *did.NewVerificationMethodFromBytes("did:example:123456789abcdefghi#keys-1",
+			"Secp256k1VerificationKey2018",
+			"did:example:123456789abcdefghi",
+			base58.Decode("H3C2AVvLMv6gmMNam3uVAjZpfkcJCwDwnZn6z3wXmqPV")), Relationship: did.Authentication},
+		{VerificationMethod: did.VerificationMethod{
+			ID:         "did:example:123456789abcdefghs#key3",
+			Controller: "did:example:123456789abcdefghs",
+			Type:       "RsaVerificationKey2018",
+			Value:      hexDecodeValue,
+		}, Relationship: did.Authentication, Embedded: true},
+	}
+
+	require.Equal(t, eAuthentication, doc.Authentication)
+
+	docBz, err := doc.JSONBytes()
+	require.NoError(t, err)
+
+	doc2, err := did.ParseDocument(docBz)
+	require.NoError(t, err)
+	require.NotNil(t, doc2)
+	require.Equal(t, doc, doc2)
+
+}
+
+const validDoc = `{
+  "@context": ["https://w3id.org/did/v1"],
+  "id": "did:example:21tDAKCERh95uGgKbJNHYp",
+  "verificationMethod": [
+    {
+      "id": "did:example:123456789abcdefghi#keys-1",
+      "type": "Secp256k1VerificationKey2018",
+      "controller": "did:example:123456789abcdefghi",
+      "publicKeyBase58": "H3C2AVvLMv6gmMNam3uVAjZpfkcJCwDwnZn6z3wXmqPV"
+    }
+  ],
+  "authentication": [
+    "did:example:123456789abcdefghi#keys-1",
+    {
+      "id": "did:example:123456789abcdefghs#key3",
+      "type": "RsaVerificationKey2018",
+      "controller": "did:example:123456789abcdefghs",
+      "publicKeyHex": "02b97c30de767f084ce3080168ee293053ba33b235d7116a3263d29f1450936b71"
+    }
+  ],
+  "created": "2002-10-10T17:00:00Z"
+}`
+
+type httpRemoteDocumentLoader struct{}
+
+func (l *httpRemoteDocumentLoader) LoadDocument(url string) (*jsld.RemoteDocument, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	doc, err := jsld.DocumentFromReader(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return &jsld.RemoteDocument{
+		DocumentURL: url,
+		Document:    doc,
+	}, nil
+}
+
+func TestSign(t *testing.T) {
+	// make document loader
+	storeProvider := mem.NewProvider()
+	contextStore, err := ldstore.NewContextStore(storeProvider)
+	require.NoError(t, err)
+	remoteProviderStore, err := ldstore.NewRemoteProviderStore(storeProvider)
+	require.NoError(t, err)
+
+	ctx, err := context.New(
+		context.WithJSONLDContextStore(contextStore),
+		context.WithJSONLDRemoteProviderStore(remoteProviderStore),
+	)
+	require.NoError(t, err)
+
+	loader, err := ld.NewDocumentLoader(
+		ctx,
+		ld.WithRemoteDocumentLoader(&httpRemoteDocumentLoader{}),
+	)
+	require.NoError(t, err)
+
+	// make doc sample
+
+	privKey, err := ecdsa.GenerateKey(btcec.S256(), rand.Reader)
+	require.NoError(t, err)
+	pubKey := &privKey.PublicKey
+	pubKeyBytes := elliptic.Marshal(pubKey.Curve, pubKey.X, pubKey.Y)
+
+	//privKey := secp256k1.GenPrivKey()
+	//pubKey := privKey.PubKey()
+	// pubKeyBytes := pubKey.Bytes()
+
+	const (
+		didContext      = "https://w3id.org/did/v1"
+		securityContext = "https://w3id.org/security/v1"
+	)
+	vm := did.VerificationMethod{
+		ID:         "did:method:abc" + "#key1",
+		Type:       "EcdsaSecp256k1VerificationKey2019",
+		Controller: "did:method:abc",
+		Value:      pubKeyBytes,
+	}
+	createdTime := time.Now()
+
+	signerContext := &signer.Context{
+		Creator:       "did:method:abc" + "#key1",
+		SignatureType: "EcdsaSecp256k1Signature2019",
+	}
+
+	didDoc := &did.Doc{
+		Context:            []string{didContext, securityContext},
+		ID:                 "did:method:abc",
+		VerificationMethod: []did.VerificationMethod{vm},
+		Created:            &createdTime,
+	}
+
+	jsonDoc, err := didDoc.JSONBytes()
+	require.NoError(t, err)
+
+	// sign doc
+	docSigner := signer.New(ecdsasecp256k1signature2019.New(
+		suite.WithSigner(getSigner(privKey))))
+
+	signedDoc, err := docSigner.Sign(signerContext, jsonDoc, jsonld.WithDocumentLoader(loader))
+	require.NoError(t, err)
+	//sigVerifier := ecdsasecp256k1signature2019.New(suite.WithSigner(newSecp256k1Signer(privKey)))
+	sigVerifier := ecdsasecp256k1signature2019.New(suite.WithVerifier(ecdsasecp256k1signature2019.NewPublicKeyVerifier()))
+
+	doc, err := did.ParseDocument(signedDoc)
+	require.Nil(t, err)
+	require.NotNil(t, doc)
+	err = doc.VerifyProof([]verifier.SignatureSuite{sigVerifier}, jsonld.WithDocumentLoader(loader))
+	require.NoError(t, err)
+
+	doc.Proof[0].ProofValue = []byte("invalid")
+	err = doc.VerifyProof([]verifier.SignatureSuite{sigVerifier}, jsonld.WithDocumentLoader(loader))
+	require.NotNil(t, err)
+}
+
+func getSigner(privKey *ecdsa.PrivateKey) *testSigner {
+	return &testSigner{privateKey: privKey}
+}
+
+//type testSigner struct {
+//	privateKey secp256k1.PrivKey
+//}
+
+//func (s *testSigner) Sign(doc []byte) ([]byte, error) {
+//	priv, _ := btcec.PrivKeyFromBytes(btcec.S256(), s.privateKey)
+//	sig, err := priv.Sign(getSHA256(doc))
+//	if err != nil {
+//		return nil, fmt.Errorf("failed to sign: %w", err)
+//	}
+//	return serializeSig(sig), nil
+//}
+
+type testSigner struct {
+	privateKey *ecdsa.PrivateKey
+}
+
+func (signer *testSigner) Sign(doc []byte) ([]byte, error) {
+	hasher := crypto.SHA256.New()
+	_, _ = hasher.Write(doc) //nolint:errcheck
+	hashed := hasher.Sum(nil)
+
+	r, s, err := ecdsa.Sign(rand.Reader, signer.privateKey, hashed)
+	if err != nil {
+		return nil, err
+	}
+
+	curveBits := signer.privateKey.Curve.Params().BitSize
+
+	keyBytes := curveBits / 8
+	if curveBits%8 > 0 {
+		keyBytes++
+	}
+
+	copyPadded := func(source []byte, size int) []byte {
+		dest := make([]byte, size)
+		copy(dest[size-len(source):], source)
+
+		return dest
+	}
+	return append(copyPadded(r.Bytes(), keyBytes), copyPadded(s.Bytes(), keyBytes)...), nil
+
+}
+
+func getSHA256(bytes []byte) []byte {
+	hasher := sha256.New()
+	hasher.Write(bytes)
+	return hasher.Sum(nil)
+}
+
+func serializeSig(sig *btcec.Signature) []byte {
+	rBytes := sig.R.Bytes()
+	sBytes := sig.S.Bytes()
+	sigBytes := make([]byte, 64)
+	// 0 pad the byte arrays from the left if they aren't big enough.
+	copy(sigBytes[32-len(rBytes):32], rBytes)
+	copy(sigBytes[64-len(sBytes):64], sBytes)
+	return sigBytes
+}
