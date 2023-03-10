2b40912b665d49659e9ddbbdb82bd23be304bd8e
audtlr24 <audtlr24@gmail.com>
2023-03-10 19:06:09 +0900

    Revert "feat: add what-is-dep"
    This reverts commit e8f322d4678289f5cf7db532c804294740f25ac3.

--- init.sh
+++
@@ -1,8 +0,0 @@
-rm -rf ~/.panacea
-panacead init local --chain-id local
-
-panacead keys add oracle1
-panacead add-genesis-account $(panacead keys show oracle1 -a) 100000000000umed
-panacead gentx oracle1 100000000umed --commission-rate 0.1 --commission-max-rate 0.2 --commission-max-change-rate 0.01  --min-self-delegation 1 --chain-id local
-
-panacead collect-gentxs
