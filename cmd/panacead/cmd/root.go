618f99056e30155aa1cc01131d228271563e296b
InChul <49394875+inchori@users.noreply.github.com>
2023-03-22 14:59:07 +0900

    feat: add a `panacead dep hash-data` command (#654)

--- cmd/panacead/cmd/root.go
+++ cmd/panacead/cmd/root.go
@@ -193,6 +193,7 @@ func depCommand(defaultNodeHome string) *cobra.Command {
 		dep.EncryptDataCmd(defaultNodeHome),
 		dep.DecryptDataCmd(defaultNodeHome),
 		dep.JwtCmd(defaultNodeHome),
+		dep.HashJSONCmd(),
 	)
 
 	cmd.PersistentFlags().String(flags.FlagChainID, "", "The network chain ID")
