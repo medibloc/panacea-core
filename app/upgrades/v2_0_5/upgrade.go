63e3cdea6b0e8ac7773b66ff764223832e0f13b5
Minkyoo Cha <minkyoo.cha@medibloc.org>
2026-06-30 14:40:32 +0900

    chore: update IBC imports to v8

--- app/upgrades/v2_0_5/upgrade.go
+++ app/upgrades/v2_0_5/upgrade.go
@@ -4,7 +4,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
-	ibcconnectiontypes "github.com/cosmos/ibc-go/v7/modules/core/03-connection/types"
+	ibcconnectiontypes "github.com/cosmos/ibc-go/v8/modules/core/03-connection/types"
 	"github.com/medibloc/panacea-core/v2/app/keepers"
 )
 
