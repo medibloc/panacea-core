7577db075ed8f232a96ca750aaad0e7336d4eb89
Minkyoo Cha <minkyoo.cha@medibloc.org>
2026-07-13 17:10:00 +0900

    test(cli): cover application genesis init

--- cmd/panacead/cmd/root_test.go
+++ cmd/panacead/cmd/root_test.go
@@ -1,8 +1,14 @@
 package cmd_test
 
 import (
+	"encoding/json"
+	"io"
+	"path/filepath"
 	"testing"
+	"time"
 
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+	genutiltypes "github.com/cosmos/cosmos-sdk/x/genutil/types"
 	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/require"
 
@@ -52,6 +58,36 @@ func TestNewRootCmdCommandTree(t *testing.T) {
 
 	t.Run("has unique command names", func(t *testing.T) {
 		requireUniqueChildNames(t, root)
+	})
+
+	t.Run("init writes application genesis", func(t *testing.T) {
+		home := t.TempDir()
+		root.SetArgs([]string{
+			"init",
+			"test-node",
+			"--home", home,
+			"--chain-id", "test-chain",
+			"--initial-height", "7",
+		})
+		root.SetOut(io.Discard)
+		root.SetErr(io.Discard)
+
+		require.NoError(t, svrcmd.Execute(root, "", home))
+
+		appGenesis, err := genutiltypes.AppGenesisFromFile(filepath.Join(home, "config", "genesis.json"))
+		require.NoError(t, err)
+		require.Equal(t, "test-chain", appGenesis.ChainID)
+		require.Equal(t, int64(7), appGenesis.InitialHeight)
+		require.False(t, appGenesis.GenesisTime.IsZero())
+		require.NotNil(t, appGenesis.Consensus)
+		require.NotNil(t, appGenesis.Consensus.Params)
+		require.Empty(t, appGenesis.Consensus.Validators)
+		require.Equal(t, 21*24*time.Hour, appGenesis.Consensus.Params.Evidence.MaxAgeDuration)
+		require.Equal(t, int64(21*24*time.Hour/(6*time.Second)), appGenesis.Consensus.Params.Evidence.MaxAgeNumBlocks)
+
+		var appState map[string]json.RawMessage
+		require.NoError(t, json.Unmarshal(appGenesis.AppState, &appState))
+		require.NotEmpty(t, appState)
 	})
 }
 
