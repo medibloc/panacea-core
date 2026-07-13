b333648f6f74b80ff73c502c8cc00ca9860fcb50
Minkyoo Cha <minkyoo.cha@medibloc.org>
2026-07-13 14:36:17 +0900

    fix(upgrade): adapt v2.0.5 handler context

--- app/upgrades/v2_0_5/upgrade.go
+++ app/upgrades/v2_0_5/upgrade.go
@@ -1,6 +1,8 @@
 package v2_0_5
 
 import (
+	"context"
+
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
@@ -9,7 +11,9 @@ import (
 )
 
 func CreateUpgradeHandle(mm *module.Manager, configurator module.Configurator, keepers *keepers.AppKeepersWithKey) upgradetypes.UpgradeHandler {
-	return func(ctx sdk.Context, plan upgradetypes.Plan, _ module.VersionMap) (module.VersionMap, error) {
+	return func(ctx context.Context, plan upgradetypes.Plan, _ module.VersionMap) (module.VersionMap, error) {
+		sdkCtx := sdk.UnwrapSDKContext(ctx)
+
 		// 1st-time running in-store migrations, using 1 as fromVersion to
 		// avoid running InitGenesis.
 		fromVM := map[string]uint64{
@@ -35,7 +39,7 @@ func CreateUpgradeHandle(mm *module.Manager, configurator module.Configurator, k
 			"wasm":         1,
 		}
 
-		keepers.IBCKeeper.ConnectionKeeper.SetParams(ctx, ibcconnectiontypes.DefaultParams())
+		keepers.IBCKeeper.ConnectionKeeper.SetParams(sdkCtx, ibcconnectiontypes.DefaultParams())
 
 		return mm.RunMigrations(ctx, configurator, fromVM)
 	}
