22e1b4a679a78526140e11842730825e0e6ab3f0
Minkyoo Cha <minkyoo.cha@medibloc.org>
2026-07-13 23:31:59 +0900

    test(pnft): verify standalone nft keeper wiring

--- app/app_test.go
+++ app/app_test.go
@@ -1,11 +1,16 @@
 package app_test
 
 import (
+	"bytes"
 	"testing"
+	"time"
 
 	"cosmossdk.io/log"
+	"cosmossdk.io/x/nft"
+	cmtproto "github.com/cometbft/cometbft/proto/tendermint/types"
 	dbm "github.com/cosmos/cosmos-db"
 	"github.com/cosmos/cosmos-sdk/client/flags"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	capabilitytypes "github.com/cosmos/ibc-go/modules/capability/types"
 	ibctransfertypes "github.com/cosmos/ibc-go/v8/modules/apps/transfer/types"
 	ibcexported "github.com/cosmos/ibc-go/v8/modules/core/exported"
@@ -14,6 +19,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	panaceaapp "github.com/medibloc/panacea-core/v2/app"
+	pnfttypes "github.com/medibloc/panacea-core/v2/x/pnft/types"
 )
 
 func TestRuntimeModulesHaveBootstrapBasics(t *testing.T) {
@@ -62,4 +68,52 @@ func TestCapabilityWiring(t *testing.T) {
 	require.Equal(t, capabilitytypes.ModuleName, testApp.ModuleManager.OrderInitGenesis[0])
 	require.NotEmpty(t, testApp.ModuleManager.OrderExportGenesis)
 	require.Equal(t, capabilitytypes.ModuleName, testApp.ModuleManager.OrderExportGenesis[0])
+}
+
+func TestPNFTWiringWithStandaloneNFTKeeper(t *testing.T) {
+	panaceaapp.SetConfig()
+	appOpts := viper.New()
+	appOpts.Set(flags.FlagHome, t.TempDir())
+	testApp := panaceaapp.New(log.NewNopLogger(), dbm.NewMemDB(), nil, false, appOpts)
+	require.NoError(t, testApp.LoadLatestVersion())
+
+	require.Contains(t, testApp.GetKVStoreKey(), pnfttypes.StoreKey)
+	require.NotContains(t, testApp.GetKVStoreKey(), nft.ModuleName)
+	require.Contains(t, testApp.ModuleManager.Modules, pnfttypes.ModuleName)
+	require.NotContains(t, testApp.ModuleManager.Modules, nft.ModuleName)
+
+	ctx := testApp.NewUncachedContext(false, cmtproto.Header{Time: time.Now()})
+	owner := sdk.AccAddress(bytes.Repeat([]byte{1}, 20)).String()
+	receiver := sdk.AccAddress(bytes.Repeat([]byte{2}, 20)).String()
+
+	denom := &pnfttypes.Denom{
+		Id:     "test-denom",
+		Name:   "Test Denom",
+		Symbol: "TEST",
+		Owner:  owner,
+	}
+	require.NoError(t, testApp.PnftKeeper.SaveDenom(ctx, denom))
+
+	token := &pnfttypes.Pnft{
+		DenomId:   denom.Id,
+		Id:        "test-pnft",
+		Name:      "Test PNFT",
+		Creator:   owner,
+		Owner:     owner,
+		CreatedAt: time.Now(),
+	}
+	require.NoError(t, testApp.PnftKeeper.MintPNFT(ctx, token))
+
+	stored, err := testApp.PnftKeeper.GetPNFT(ctx, denom.Id, token.Id)
+	require.NoError(t, err)
+	require.Equal(t, owner, stored.Owner)
+
+	require.NoError(t, testApp.PnftKeeper.TransferPNFT(ctx, denom.Id, token.Id, owner, receiver))
+	stored, err = testApp.PnftKeeper.GetPNFT(ctx, denom.Id, token.Id)
+	require.NoError(t, err)
+	require.Equal(t, receiver, stored.Owner)
+
+	require.NoError(t, testApp.PnftKeeper.BurnPNFT(ctx, denom.Id, token.Id, receiver))
+	_, err = testApp.PnftKeeper.GetPNFT(ctx, denom.Id, token.Id)
+	require.Error(t, err)
 }
