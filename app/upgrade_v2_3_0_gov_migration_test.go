913e4e292734d381021bda5f42675477126ed2a9
Minkyoo Cha <minkyoo.cha@medibloc.org>
2026-08-02 09:36:01 +0900

    fix(app): configure expedited governance deposit on upgrade

---
+++ app/upgrade_v2_3_0_gov_migration_test.go
@@ -0,0 +1,75 @@
+package app_test
+
+import (
+	"testing"
+	"time"
+
+	"cosmossdk.io/core/header"
+	"cosmossdk.io/log"
+	"cosmossdk.io/math"
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+	abci "github.com/cometbft/cometbft/abci/types"
+	cmtproto "github.com/cometbft/cometbft/proto/tendermint/types"
+	dbm "github.com/cosmos/cosmos-db"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	govv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/require"
+
+	panaceaapp "github.com/medibloc/panacea-core/v2/app"
+	"github.com/medibloc/panacea-core/v2/app/upgrades/v2_3_0"
+	"github.com/medibloc/panacea-core/v2/types/assets"
+)
+
+func TestV230UpgradeConfiguresExpeditedGovernanceDeposit(t *testing.T) {
+	panaceaapp.SetConfig()
+	appOpts := viper.New()
+	appOpts.Set(flags.FlagHome, t.TempDir())
+	testApp := panaceaapp.New(log.NewNopLogger(), dbm.NewMemDB(), nil, false, appOpts)
+	require.NoError(t, testApp.LoadLatestVersion())
+
+	const upgradeHeight = int64(10)
+	blockTime := time.Unix(1_700_000_000, 0).UTC()
+	ctx := testApp.NewUncachedContext(
+		false,
+		cmtproto.Header{Height: upgradeHeight, Time: blockTime},
+	).WithHeaderInfo(header.Info{Height: upgradeHeight, Time: blockTime})
+
+	fromVM := testApp.ModuleManager.GetVersionMap()
+	fromVM[govtypes.ModuleName] = 4
+	require.NoError(t, testApp.UpgradeKeeper.SetModuleVersionMap(ctx, fromVM))
+
+	expectedMinDeposit := sdk.NewCoins(sdk.NewInt64Coin(assets.MicroMedDenom, 100_000_000_000))
+	legacyParams := govv1.DefaultParams()
+	legacyParams.MinDeposit = expectedMinDeposit
+	legacyParams.ExpeditedMinDeposit = nil
+	legacyParams.ExpeditedVotingPeriod = nil
+	legacyParams.ExpeditedThreshold = ""
+	legacyParams.ProposalCancelRatio = ""
+	legacyParams.ProposalCancelDest = ""
+	legacyParams.MinDepositRatio = ""
+	require.NoError(t, testApp.GovKeeper.Params.Set(ctx, legacyParams))
+
+	require.NoError(t, testApp.UpgradeKeeper.ScheduleUpgrade(ctx, upgradetypes.Plan{
+		Name:   v2_3_0.UpgradeName,
+		Height: upgradeHeight,
+	}))
+	_, err := testApp.PreBlocker(ctx, &abci.RequestFinalizeBlock{Height: upgradeHeight})
+	require.NoError(t, err)
+
+	toVM, err := testApp.UpgradeKeeper.GetModuleVersionMap(ctx)
+	require.NoError(t, err)
+	require.Equal(t, uint64(5), toVM[govtypes.ModuleName])
+
+	params, err := testApp.GovKeeper.Params.Get(ctx)
+	require.NoError(t, err)
+	require.Equal(t, expectedMinDeposit, sdk.Coins(params.MinDeposit))
+	require.Equal(
+		t,
+		expectedMinDeposit.MulInt(math.NewInt(govv1.DefaultMinExpeditedDepositTokensRatio)),
+		sdk.Coins(params.ExpeditedMinDeposit),
+	)
+	require.NoError(t, params.ValidateBasic())
+}
--- app/upgrades/v2_3_0/upgrade.go
+++ app/upgrades/v2_3_0/upgrade.go
@@ -3,13 +3,37 @@ package v2_3_0
 import (
 	"context"
 
+	"cosmossdk.io/math"
 	upgradetypes "cosmossdk.io/x/upgrade/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	govv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
 	"github.com/medibloc/panacea-core/v2/app/keepers"
 )
 
-func CreateUpgradeHandler(mm *module.Manager, configurator module.Configurator, _ *keepers.AppKeepersWithKey) upgradetypes.UpgradeHandler {
+func CreateUpgradeHandler(mm *module.Manager, configurator module.Configurator, keepers *keepers.AppKeepersWithKey) upgradetypes.UpgradeHandler {
 	return func(ctx context.Context, _ upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		return mm.RunMigrations(ctx, configurator, fromVM)
+		toVM, err := mm.RunMigrations(ctx, configurator, fromVM)
+		if err != nil {
+			return fromVM, err
+		}
+
+		if fromVersion, exists := fromVM[govtypes.ModuleName]; !exists || fromVersion >= toVM[govtypes.ModuleName] {
+			return toVM, nil
+		}
+
+		params, err := keepers.GovKeeper.Params.Get(ctx)
+		if err != nil {
+			return fromVM, err
+		}
+		params.ExpeditedMinDeposit = sdk.Coins(params.MinDeposit).MulInt(
+			math.NewInt(govv1.DefaultMinExpeditedDepositTokensRatio),
+		)
+		if err := keepers.GovKeeper.Params.Set(ctx, params); err != nil {
+			return fromVM, err
+		}
+
+		return toVM, nil
 	}
 }
